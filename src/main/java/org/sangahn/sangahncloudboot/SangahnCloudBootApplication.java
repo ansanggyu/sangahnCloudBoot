feb35abd9330500efafb3ba6a4fbb3cb11ec1a2d
AHN <gd20624@gmail.com>
2025-01-10 11:53:43 +0900

    Initial commit

---
+++ src/main/java/org/sangahn/sangahncloudboot/SangahnCloudBootApplication.java
@@ -0,0 +1,13 @@
+package org.sangahn.sangahncloudboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangahnCloudBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangahnCloudBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sangahn/sangahncloudboot/SangahnCloudBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sangahn.sangahncloudboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangahnCloudBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
